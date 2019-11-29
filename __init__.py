80bd4fdab6ab1b98758b8733e4d7a5a9d4471086
BenjaminSauder <13512160+BenjaminSauder@users.noreply.github.com>
2019-11-29 11:41:19 +0100

    * shows collection names of all selected objects

--- __init__.py
+++ __init__.py
@@ -3,56 +3,52 @@ import bgl
 import blf
 
 from bpy.app.handlers import persistent
-from time import perf_counter 
+from time import perf_counter
 
 bl_info = {
-    "name": "DisplayCollectionNames",
+    "name": "Display Collection Names",
     "description": "Prints the collections of the active object into the 3D View.",
     "author": "Benajmin Sauder",
     "version": (1, 0),
-    "blender": (2, 81, 0),   
+    "blender": (2, 81, 0),
+    "location": "View3D",
+    "category": "3D View"
 }
 
 
 class Updater():
     '''
     This just encapsulates all relevant data, and a few helper methods to extract placement and texts.
-    '''    
+    '''
+
     collections_text = ""
     active_object = None
-    
+
     last_update = 0
     x = 0
     y = 0
-
 
     def update_collection_names(self):
         selected_objects = bpy.context.selected_objects
 
         if len(selected_objects) == 0:
-            self.active_object = None
             self.collections_text = ""
             return False
 
-        active_object = bpy.context.view_layer.objects.active
-        
-        if not active_object:
-            self.active_object = None
-            self.collections_text = ""
-            return False
-        
-        self.active_object = active_object
+        collection_names = set()
+        for obj in selected_objects:
+            collections = [c.name_full for c in obj.users_collection]
+            collection_names.update(collections)
 
-        collections = [c.name_full for c in active_object.users_collection]        
-        collections_text = " | ".join(collections)
-
-        self.collections_text = collections_text
+        self.collections_text = " | ".join(collection_names)
 
         return True
 
-
-    # this is a bit weak, lets hope its good enough...
     def find_region(self, width, height):
+        ''' 
+        As I know no good way to get the correct context into the draw call back,
+        I do this viewport size comparison - its a bit weak, lets hope its good enough...
+        '''
         context = bpy.context
         for window in context.window_manager.windows:
             for area in window.screen.areas:
@@ -64,10 +60,9 @@ class Updater():
                                 return region, area
         return None, None
 
-
     def update_placement(self, do_update):
         if not do_update and perf_counter() > (self.last_update + 0.25):
-            return        
+            return
 
         viewport_info = bgl.Buffer(bgl.GL_INT, 4)
         bgl.glGetIntegerv(bgl.GL_VIEWPORT, viewport_info)
@@ -78,7 +73,7 @@ class Updater():
         x_offset = 0
         y_offset = 0
 
-        region, area = self.find_region(width, height)        
+        region, area = self.find_region(width, height)
         if region:
             for region in area.regions:
                 if region.type == 'TOOLS':
@@ -87,13 +82,12 @@ class Updater():
                     y_offset = region.height
                 if region.type == 'TOOL_HEADER':
                     y_offset = region.height
-     
+
         self.x = x_offset + 18
-        self.y = height - y_offset - 60    
-        self.last_update = perf_counter() 
+        self.y = height - y_offset - 60
+        self.last_update = perf_counter()
 
-
-    def draw(self, dummy): 
+    def draw(self, dummy):
         if not bpy.context.space_data.overlay.show_text:
             return
 
@@ -103,30 +97,33 @@ class Updater():
         font_id = 0
 
         bgl.glEnable(bgl.GL_BLEND)
-        blf.enable(font_id, blf.SHADOW )  
-       
+        blf.enable(font_id, blf.SHADOW)
+
         blf.color(font_id, 1.0, 1.0, 1.0, 1)
         blf.size(font_id, 11, 72)
         blf.position(0, self.x, self.y, 0)
 
         blf.draw(font_id, self.collections_text)
 
-        blf.disable(font_id, blf.SHADOW )
+        blf.disable(font_id, blf.SHADOW)
         bgl.glDisable(bgl.GL_BLEND)
 
 
 updater = Updater()
 
+
 def register():
-    args = (updater,)    
-    updater.draw_handler = bpy.types.SpaceView3D.draw_handler_add(updater.draw, args, 'WINDOW', 'POST_PIXEL')
+    args = (updater,)
+    updater.draw_handler = bpy.types.SpaceView3D.draw_handler_add(
+        updater.draw, args, 'WINDOW', 'POST_PIXEL')
 
 
-def unregister():    
+def unregister():
     if updater.draw_handler:
-        bpy.types.SpaceView3D.draw_handler_remove(updater.draw_handler, 'WINDOW')
+        bpy.types.SpaceView3D.draw_handler_remove(
+            updater.draw_handler, 'WINDOW')
     updater.draw_handler = None
 
 
 if __name__ == "__main__":
-    register() 
+    register()
